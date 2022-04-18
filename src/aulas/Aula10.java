5d714347c0491d4faeeb7180581b5da4a60f01d8
Hugo Brendow <hugobrendow97@gmail.com>
2022-04-18 20:56:54 -0300

    Alterando arquivos

---
+++ src/aulas/Aula10.java
@@ -0,0 +1,7 @@
+package aulas;
+
+public class Aula10 {
+    public static void main(String[] args) {
+        System.out.println("Aula 10");
+    }
+}
--- src/aulas/AulaGit2.java
+++ src/aulas/AulaGit2.java
@@ -2,6 +2,7 @@ package aulas;
 
 public class AulaGit2 {
     public static void main(String[] args) {
-        System.out.println("Aula git 2");
+        System.out.println("Aula git 2 e modificação");
+        System.out.println("Adicionando outra linha");
     }
 }
